d273fa3b5a8422d1d7edef7ea6ef2aa0196adb53
AvivaRubenstein <avivadrubenstein@gmail.com>
2023-02-11 21:17:17 -0500

    cleanup

--- index.js
+++ index.js
@@ -32,71 +32,71 @@ const menuQ = [{
     name: 'menu',
 }];
 var addDeptQ = [
-{
-    type: 'input',
-    message: "Department name: ",
-    name: 'newDeptName',
-}];
+    {
+        type: 'input',
+        message: "Department name: ",
+        name: 'newDeptName',
+    }];
 var addRoleQs = [
-{
-    type: 'input',
-    message: "What is the name of the new role?",
-    name: 'newRoleName',
-},
-{
-    type: 'input',
-    message: "What is the salary for the new role?",
-    name: 'newRoleSalary',
-},
-{
-    type: 'list',
-    message: "What department is the new role under?",
-    choices: departmentsArray,
-    name: 'newRoleDept',
-} ];
+    {
+        type: 'input',
+        message: "What is the name of the new role?",
+        name: 'newRoleName',
+    },
+    {
+        type: 'input',
+        message: "What is the salary for the new role?",
+        name: 'newRoleSalary',
+    },
+    {
+        type: 'list',
+        message: "What department is the new role under?",
+        choices: departmentsArray,
+        name: 'newRoleDept',
+    }];
 var addEmployeeQs = [
-{
-    type: 'input',
-    message: "What is the employee's first name?",
-    name: 'newEmployeeFirstName',
+    {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'newEmployeeFirstName',
 
-},
-{
-    type: 'input',
-    message: "What is the employee's last name?",
-    name: 'newEmployeeLastName',
-},
-{
-    type: 'list',
-    message: "What is the employee's role?",
-    choices: rolesArray,
-    name: 'newEmployeeRole',
-   
-},
-{
-    type: 'list',
-    message: "Who is the employee's manager?",
-    choices: managersArray,
-    name: 'newEmployeeManager',
-    
-} ];
+    },
+    {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'newEmployeeLastName',
+    },
+    {
+        type: 'list',
+        message: "What is the employee's role?",
+        choices: rolesArray,
+        name: 'newEmployeeRole',
+
+    },
+    {
+        type: 'list',
+        message: "Who is the employee's manager?",
+        choices: managersArray,
+        name: 'newEmployeeManager',
+
+    }];
 var updateEmployeeQs = [
-{
-    //TODO: update the choices to include all existing employees
-    type: 'list',
-    message: "Please select which Employee you would like to update:",
-    choices: employeesArray,
-    name: 'selectEmployeeToUpdate',
-   
-},
-{
-    //TODO: update the choices to include all existing role
-    type: 'list',
-    message: "What is this employee's new role?",
-    choices: rolesArray,
-    name: 'updateRole',
-  
-},
+    {
+        //TODO: update the choices to include all existing employees
+        type: 'list',
+        message: "Please select which Employee you would like to update:",
+        choices: employeesArray,
+        name: 'selectEmployeeToUpdate',
+
+    },
+    {
+        //TODO: update the choices to include all existing role
+        type: 'list',
+        message: "What is this employee's new role?",
+        choices: rolesArray,
+        name: 'updateRole',
+
+    },
 ];
 
 
@@ -134,7 +134,7 @@ function init() {
                     //to close the application
                     process.exit();
                     break;
-                
+
             }
         });
 }
@@ -153,15 +153,16 @@ function viewRoles() {
         //destructuring with mapping
         //we need an array of roles to be the choices in the prompt questions
         rolesArray = [];
-        rolesArray = result.map((res)=> {
-            return {role: res.Job_Title,
+        rolesArray = result.map((res) => {
+            return {
+                role: res.Job_Title,
                 department: res.Department,
-                 salary : res.Salary
+                salary: res.Salary
             }
-            
+
         });
-         console.table(result);
-         init();
+        console.table(result);
+        init();
         // console.log(result);
         //console.log(rolesArray);
     });
@@ -180,7 +181,7 @@ function viewEmployees() {
     JOIN roles ON employee.role_id = roles.id
     JOIN department ON roles.department_id = department.id
     LEFT JOIN employee AS manager ON employee.manager_id = manager.id`, (err, result) => {
-        employeesArray = result.map((res)=> {
+        employeesArray = result.map((res) => {
             return {
                 id: res.Employee_ID,
                 name: res.First_Name + " " + res.Last_Name,
@@ -188,7 +189,7 @@ function viewEmployees() {
                 department: res.Department,
                 salary: res.Salary,
                 manager: res.Manager_first + " " + res.Manager_last,
-               
+
             }
         });
         console.table(result);
@@ -204,54 +205,54 @@ function viewEmployeesByManagers() {
     FROM employee
     JOIN employee AS manager ON employee.manager_id = manager.id
     ORDER BY employee.manager_id;`, (err, result) => {
-    console.table(result); 
-    init();}
-     );
-   
+        console.table(result);
+        init();
+    }
+    );
+
 }
 
 function addDepartment() {
     inquirer.prompt(addDeptQ)
-        .then((answers) => { 
+        .then((answers) => {
             db.query(`INSERT INTO department (d_name) VALUES ("${answers.newDeptName}")`);
             console.log(`${answers.newDeptName} department added.`);
             populateDepartmentsArray();
             init();
-        } );
-   
+        });
+
 }
 function addRole() {
     //we are querying for the department name and id, and then pushing those results into an array
     db.query('SELECT id, d_name AS Department FROM department', (err, result) => {
         departmentsArray = result.flatMap((res) => {
-            return {id: res.id,
-                    name: res.Department};
+            return {
+                id: res.id,
+                name: res.Department
+            };
         });
         //console.log(departmentsArray);
-    //we are taking the array of departments created from the query above, and setting the choices of the relevant question
-    //to be the contents of that array
+        //we are taking the array of departments created from the query above, and setting the choices of the relevant question
+        //to be the contents of that array
         addRoleQs[2].choices = departmentsArray;
-    //then we actually prompt the user with the questions about adding a role (with the choices prepopulated from the array above)
+        //then we actually prompt the user with the questions about adding a role (with the choices prepopulated from the array above)
         inquirer.prompt(addRoleQs)
-    //and then we can do a query to insert the new role into the roles table, with all of the associated information
-        .then((answers) => {
-         db.query(`SELECT id from department WHERE d_name = "${answers.newRoleDept}"`, (err, res) => {
-            deptId = res[0].id;
-            // console.log(res[0].id);
-        
-        db.query(`INSERT INTO roles(title, salary, department_id) VALUES ("${answers.newRoleName}", "${answers.newRoleSalary}", ${deptId})`, (err, res) => {
-            console.log(`${answers.newRoleName} role added.`);
-            init();
-        });
-     
+            //and then we can do a query to insert the new role into the roles table, with all of the associated information
+            .then((answers) => {
+                db.query(`SELECT id from department WHERE d_name = "${answers.newRoleDept}"`, (err, res) => {
+                    deptId = res[0].id;
+                    // console.log(res[0].id);
+
+                    db.query(`INSERT INTO roles(title, salary, department_id) VALUES ("${answers.newRoleName}", "${answers.newRoleSalary}", ${deptId})`, (err, res) => {
+                        console.log(`${answers.newRoleName} role added.`);
+                        init();
+                    });
+
+                });
+            
+            });
     });
-    // inquirer.prompt(addRoleQs)
-    // .then((answers) => {
-    // db.query(`INSERT INTO roles(title, salary, department_id) VALUES ("${answers.newRoleName}", "${answers.newRoleSalary}", "${answers.newRoleDept}")`);
-    // console.log(`${answers.newRoleName} role added.`);
-    
-});
-});}
+}
 
 function addEmployee() {
     //need to ask about the role and the manager!
@@ -267,21 +268,23 @@ function addEmployee() {
     JOIN roles ON employee.role_id = roles.id
     JOIN department ON roles.department_id = department.id
     LEFT JOIN employee AS manager ON employee.manager_id = manager.id`, (err, result) => {
-        employeesArray = result.flatMap((res)=> {
-            return {id: res.Employee_ID,
-                    name: res.First_Name + " " + res.Last_Name };
-        
+        employeesArray = result.flatMap((res) => {
+            return {
+                id: res.Employee_ID,
+                name: res.First_Name + " " + res.Last_Name
+            };
+
         });
-        managersArray = result.flatMap((res)=> {
+        managersArray = result.flatMap((res) => {
             //some of the items in the array will be "null null" because there was no manager first/last names
             //we are setting those items to be empty quotes, and setting all of the items that ARE NOT null
             //so that the first and last names are joined together in the array
-            if(res.Manager_first || res.Manager_last !== null) {
-            return { name: res.Manager_first + " " + res.Manager_last};
-        }
-            else {return "";}
-            
-        
+            if (res.Manager_first || res.Manager_last !== null) {
+                return { name: res.Manager_first + " " + res.Manager_last };
+            }
+            else { return ""; }
+
+
         });
         //here we are filtering out any places where manager was set to empty quotes/ was originally null
         managersArray = managersArray.filter(manager => manager !== "");
@@ -289,36 +292,37 @@ function addEmployee() {
         // console.log(managersArray);
 
         // db.query(`SELECT roles.title AS Job_Title FROM roles;`, (err, result) => {
-            rolesArray = result.flatMap((res)=> {
-                return res.Job_Title; 
+        rolesArray = result.flatMap((res) => {
+            return res.Job_Title;
             // });
         });
-    addEmployeeQs[2].choices = rolesArray;
-    addEmployeeQs[3].choices = managersArray;
-    
-    inquirer.prompt(addEmployeeQs)
-    .then((answers)=> {
-        managerForNew = answers.newEmployeeManager.split(" ");
-        db.query(`SELECT roles.id FROM roles WHERE roles.title = "${answers.newEmployeeRole}"`, (err, result) => {
-            newRoleId = result[0].id;
-            // console.log(result[0].id);
-        
-            db.query(`SELECT employee.id FROM employee WHERE employee.first_name = "${managerForNew[0]}" AND employee.last_name = "${managerForNew[1]}"`, (err, res) =>{
-                updateManager = res[0].id;
-                // console.log(res[0].id);
+        addEmployeeQs[2].choices = rolesArray;
+        addEmployeeQs[3].choices = managersArray;
 
-                db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.newEmployeeFirstName}", "${answers.newEmployeeLastName}" ,${newRoleId}, ${updateManager})`, (req, response) => {
-                    console.log("Employee added"); });
-                    init();
-                });
-                
-                
-            }
-            );
-        });
-        
-        
-    
+        inquirer.prompt(addEmployeeQs)
+            .then((answers) => {
+                managerForNew = answers.newEmployeeManager.split(" ");
+                db.query(`SELECT roles.id FROM roles WHERE roles.title = "${answers.newEmployeeRole}"`, (err, result) => {
+                    newRoleId = result[0].id;
+                    // console.log(result[0].id);
+
+                    db.query(`SELECT employee.id FROM employee WHERE employee.first_name = "${managerForNew[0]}" AND employee.last_name = "${managerForNew[1]}"`, (err, res) => {
+                        updateManager = res[0].id;
+                        // console.log(res[0].id);
+
+                        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.newEmployeeFirstName}", "${answers.newEmployeeLastName}" ,${newRoleId}, ${updateManager})`, (req, response) => {
+                            console.log("Employee added");
+                        });
+                        init();
+                    });
+
+
+                }
+                );
+            });
+
+
+
     });
 
 }
@@ -327,98 +331,52 @@ function addEmployee() {
 //TODO: add updateEmployeeRole function here!
 function updateEmployeeRole(employee, newRole) {
     db.query(`SELECT employee.first_name AS First_Name, employee.last_name AS Last_Name, employee.id AS Employee_ID FROM employee`, (err, result) => {
-        employeesArray = result.map((res)=> {
+        employeesArray = result.map((res) => {
             return {
                 id: res.Employee_ID,
-                name: res.First_Name + " " + res.Last_Name,    
+                name: res.First_Name + " " + res.Last_Name,
             }
-    });
-    db.query(`SELECT roles.id, roles.title AS Title FROM roles`, (err, result) =>{
-        rolesArray = result.map((response)=> {
-            return {name: response.Title,
-            id: response.id}
-        } );
-        updateEmployeeQs[0].choices = employeesArray;
-        updateEmployeeQs[1].choices = rolesArray;
-        inquirer.prompt(updateEmployeeQs)
-        .then((answers)=>{
-            updateEmplName = answers.selectEmployeeToUpdate.split(" ");
-            db.query(`SELECT id FROM roles WHERE roles.title = "${answers.updateRole}"`, (err, response) =>
-            {   newRoleId = response[0].id;
-    
-                db.query(`UPDATE employee SET role_id = ${newRoleId} WHERE first_name = "${updateEmplName[0]}" AND last_name = "${updateEmplName[1]}"`, (err, result) => {
-                    console.log("Update complete!");
-                    init();
+        });
+        db.query(`SELECT roles.id, roles.title AS Title FROM roles`, (err, result) => {
+            rolesArray = result.map((response) => {
+                return {
+                    name: response.Title,
+                    id: response.id
+                }
+            });
+            updateEmployeeQs[0].choices = employeesArray;
+            updateEmployeeQs[1].choices = rolesArray;
+            inquirer.prompt(updateEmployeeQs)
+                .then((answers) => {
+                    updateEmplName = answers.selectEmployeeToUpdate.split(" ");
+                    db.query(`SELECT id FROM roles WHERE roles.title = "${answers.updateRole}"`, (err, response) => {
+                        newRoleId = response[0].id;
+
+                        db.query(`UPDATE employee SET role_id = ${newRoleId} WHERE first_name = "${updateEmplName[0]}" AND last_name = "${updateEmplName[1]}"`, (err, result) => {
+                            console.log("Update complete!");
+                            init();
+                        }
+                        );
+                    });
+
                 }
                 );
-            });
-            
         }
-         );
-    }
-);
-} );}
+        );
+    });
+}
 
-
-function populateRolesArray(){
-    db.query('SELECT roles.title AS Job_Title, department.d_name AS Department, roles.salary AS Salary FROM roles JOIN department ON roles.department_id = department.id', (err, result) => {
-        //destructuring with mapping
-        //use for updates, and use the array for choices
-        rolesArray = [];
-        //flatMap "flattens" the mapped array by 1 level, meaning in this case it will create 1 array with all of the roles 
-        //within in, as opposed to creating a larger array with smaller arrays within which each contain a role
-        rolesArray = result.flatMap((res)=> {
-            return res.Job_Title;
-        });
-        // console.log(rolesArray);
-     
-});}
 
 function populateDepartmentsArray() {
 
     db.query('SELECT id, d_name AS Department FROM department', (err, result) => {
         departmentsArray = result.flatMap((res) => {
-            return {id: res.id,
-                    name: res.Department};
+            return {
+                id: res.id,
+                name: res.Department
+            };
         });
         //console.log(departmentsArray);
-    });
-}
-
-
-function populateEmployeesAndManagersArrays() {
-    db.query(`SELECT employee.id AS Employee_ID,
-    employee.first_name AS First_Name,
-    employee.last_name AS Last_Name,
-    roles.title AS Job_Title,
-    department.d_name AS Department,
-    roles.salary AS Salary,
-    manager.first_name AS Manager_first, 
-    manager.last_name AS Manager_last
-    FROM employee
-    JOIN roles ON employee.role_id = roles.id
-    JOIN department ON roles.department_id = department.id
-    LEFT JOIN employee AS manager ON employee.manager_id = manager.id`, (err, result) => {
-        // employeesArray = [];
-        // managersArray = [];
-        employeesArray = result.flatMap((res)=> {
-            return res.First_Name + " " + res.Last_Name;
-            
-        });
-        managersArray = result.flatMap((res)=> {
-            //some of the items in the array will be "null null" because there was no manager first/last names
-            //we are setting those items to be empty quotes, and setting all of the items that ARE NOT null
-            //so that the first and last names are joined together in the array
-            if(res.Manager_first || res.Manager_last !== null) {
-            return res.Manager_first + " " + res.Manager_last;}
-            else {return "";}
-            
-        
-        });
-        //here we are filtering out any places where manager was set to empty quotes/ was originally null
-        managersArray = managersArray.filter(manager => manager !== "");
-        // console.log(employeesArray);
-        // console.log(managersArray);
     });
 }
 
